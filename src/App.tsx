8f0dfcc3ca4e7b269c18bdbc7a9efc085736c80f
Clare <chblackburne@gmail.com>
2021-05-23 16:54:32 +0100

    Basic mutation and cache w QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,16 @@
 import React from 'react';
-import { useMutation, useQuery } from 'react-query';
+import { useMutation, useQuery, useQueryClient } from 'react-query';
+import { Updater } from 'react-query/types/core/utils';
 import './App.css';
 
 interface User {
   id?: string;
   first_name?: string;
   last_name?: string;
+}
+
+interface OldDataProps {
+  oldData: Updater<{ data: User[] } | undefined, { data: User[] }>;
 }
 
 const getUsers = async () => {
@@ -31,11 +36,12 @@ const addUser = async (user: User) => {
   if (!response.ok) {
     throw new Error('Oops! Something went wrong!');
   }
-
   return response.json();
 };
 
 function App() {
+  const queryClient = useQueryClient();
+
   const {
     data: users,
     isLoading,
@@ -43,17 +49,32 @@ function App() {
     error,
     refetch,
   } = useQuery('users', getUsers);
+
   const {
     mutate,
     isLoading: addingUser,
     error: addUserError,
   } = useMutation(addUser, {
-    onSuccess: (data) => {
-      console.log(data);
-      // refetches data to update
-      refetch();
+    onSuccess: (newUser) => {
+      // refetches data to update with refetch from useQuery()
+      // refetch();
+      // queryClient.invalidateQueries('users');
+
+      // modify cache to avoid unnecessary api calls
+
+      queryClient.setQueryData('users', (oldData: any | undefined) => {
+        return {
+          ...oldData,
+          data: [newUser, ...oldData.data],
+        };
+      });
     },
   });
+
+  const handleAddNewUser = async () => {
+    const newUser = await mutate({ first_name: 'Bob', last_name: 'Smith' });
+    return newUser;
+  };
 
   if (isLoading) {
     return <p>Is loading</p>;
@@ -68,11 +89,7 @@ function App() {
       <header className="App-header"></header>
       <main>
         {addingUser ? <p>Adding user</p> : null}
-        <button
-          onClick={() => mutate({ first_name: 'Bob', last_name: 'Smith' })}
-        >
-          Add User
-        </button>
+        <button onClick={() => handleAddNewUser()}>Add User</button>
         <ul>
           {users.data.map((user: User) => (
             <li key={user?.id}>{user?.first_name}</li>
